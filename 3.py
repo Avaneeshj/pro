5a09bddc5b5229b7515eb20d01e47e0312ef9a3c
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-08-03 23:37:03 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,13 +1,8 @@
 a,b=map(list,input().split())
-c=0
-while(len(a)!=len(b)):
-    if(len(a)>len(b)):
-        b.append("@")
-    else:
-        a.append("@")
+c=abs(len(a)-len(b))
 for i in range(len(a)):
-    if a[i]==b[i]:
-        continue
-    else:
+    if len(b)==1 and b[i] in a:
+        break
+    if a[i]!=b[i]:
         c+=1
 print(c)
