a02cc7cf3f2bb3a40e3d33e1113b6c5134715e2c
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-08-08 00:43:26 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,9 +1,17 @@
 a=int(input())
 b=list(map(int,input().split()))
 c=0
+e=[]
 d=[]
 for i in range(a-1):
-    if b[i]<b[i+1]:
-        d=b[0:i+1]
-        c+=sum(d)
+    d=b[:i+2]
+    k=0
+    f=len(d)
+    while(k<f):
+        if(d[-1]>d[k]):
+            e.append(d[k])
+            k+=1
+        else:
+            k+=1
+c+=sum(e)
 print(c)
